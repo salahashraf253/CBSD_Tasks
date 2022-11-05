3dc7a7451548547d6a462035c3387f3ba27f4e47
Salah <salahashraf924@gmail.com>
2022-11-05 12:53:13 +0200

    first commit

--- React Native/Project 1/FirstProject/src/screens/LoginScreen.js
+++ React Native/Project 1/FirstProject/src/screens/LoginScreen.js
@@ -1,31 +1,31 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
 import CustomInput from "../components/CustomInput/CustomInput";
 import LoginButton from "../components/CustomButton/LoginButton";
 import CustomText from "../components/customText/customText";
 //FirstProject\src\components\Button.js
 //
 const LoginScreen = () => {
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-  const OnLoginPress = () => {
-    console.warn("sign in");
-  };
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  function OnLoginPress() {
+    alert(`Email: ${username}\nPassword: ${password}`);
+  }
   return (
     <View style={styles.container}>
       <CustomText bodyText="Enter your Email" />
       <CustomInput
         placeholder="user@email.com"
-        // value={userName}
-        // setValue={setUsername}
+        value={username}
+        setValue={setUsername}
         isPassword={false}
       />
       <CustomText bodyText="Enter your password" />
       <CustomInput
         placeholder="Password"
         isPassword={true}
-        // value={password}
-        // setValue={setPassword}
+        value={password}
+        setValue={setPassword}
       />
       <LoginButton text={"login"} onPress={OnLoginPress} />
     </View>
